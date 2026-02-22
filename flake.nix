ac80bb057f5b64d6de42999ecefe57acd1a2c6a5
Anakin Childerhose <anakin@childerhose.ca>
2026-02-22 16:41:38 -0500

    nix: add neovim master via nix flake

--- flake.nix
+++ flake.nix
@@ -1,41 +1,54 @@
 {
   description = "Neovim environment with LSPs and tools";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs";
-  outputs = { self, nixpkgs }: {
-    packages.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.buildEnv {
-      name = "nvim-env";
-      paths = with nixpkgs.legacyPackages.x86_64-linux; [
-        autotools-language-server
-        awk-language-server
-        bash-language-server
-        clang-tools
-        cmake-language-server
-        docker-language-server
-        dot-language-server
-        ginko
-        git
-        goose-cli
-        gopls
-        lazygit
-        lua-language-server
-        nil
-        oelint-adv
-        opencode
-        psmisc
-        pyright
-        rust-analyzer
-        shellcheck
-        shfmt
-        systemd-language-server
-        texlab
-        tinymist
-        tree-sitter
-        typescript-language-server
-        wl-clipboard
-        xdg-utils
-        yaml-language-server
-      ];
-    };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    # Add the neovim overlay input
+    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
   };
+
+  outputs = { self, nixpkgs, neovim-nightly-overlay }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ neovim-nightly-overlay.overlays.default ];
+      };
+    in {
+      packages.${system}.default = pkgs.buildEnv {
+        name = "nvim-env";
+        paths = with pkgs; [
+          neovim
+          autotools-language-server
+          awk-language-server
+          bash-language-server
+          clang-tools
+          cmake-language-server
+          docker-language-server
+          dot-language-server
+          ginko
+          git
+          goose-cli
+          gopls
+          lazygit
+          lua-language-server
+          nil
+          oelint-adv
+          opencode
+          psmisc
+          pyright
+          rust-analyzer
+          shellcheck
+          shfmt
+          systemd-language-server
+          texlab
+          tinymist
+          tree-sitter
+          typescript-language-server
+          wl-clipboard
+          xdg-utils
+          yaml-language-server
+        ];
+      };
+    };
 }
